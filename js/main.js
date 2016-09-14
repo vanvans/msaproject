cdede7e3be642e77419852779d3a726fec1825bd
vanvans <Vanessa@Vanessas-MacBook-Air.local>
2016-09-15 00:01:03 +1200

    api fully works

--- js/main.js
+++ js/main.js
@@ -1,20 +1,29 @@
-$(function(){
+
+$('#click').on('click', function(e) {
+    e.preventDefault();
     var $quote = $('#quote');
     $.ajax({
         type: 'GET',
-        url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
-        success: function(quote){
-            $.each(quote, function(i, item){
-                $quote.html(item.content + item.title);
+        url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
+        timeout: 3000,
+        success: function(data){
+            var post = data.shift();
+            
+            $('#quote').html(post.content);
+            $('#author').html(post.title);
+
+            },
+
+        cache: false,
         
-            });
-        }
 
     });
+
+
+
+
 });
 
 
-$("#btn-wrapper").click(function() {
-    alert( "Handler for .click() called." );
-    });
+
 

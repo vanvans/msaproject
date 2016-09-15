5b79e225fb9f74a961f209d33eb6b5f0f4e5e941
vanvans <Vanessa@Vanessas-MacBook-Air.local>
2016-09-15 20:44:46 +1200

    trying to fix deployment

--- deploy.cmd
+++ deploy.cmd
@@ -1,9 +1,9 @@
 @echo off
 
-IF "%SITE_FLAVOR%" == "nodejs" (
+IF "node.js" == "nodejs" (
   deploy.node.cmd
 ) ELSE (
-  IF "%SITE_FLAVOR%" == "mvc4" (
+  IF "nodejs" == "mvc4" (
     deploy.mvc4.cmd
   ) ELSE (
     echo You have to set SITE_FLAVOR setting to either "nodejs" or "mvc4"
